39f0ddcf5c77d5a23791391d6bfaa4f1baea7f08
Kevin Tran <k.tran787@gmail.com>
2022-12-09 14:03:52 -0800

    final commit

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function startHtml() {
     </head>
     <body>
         <nav class="navbar navbar-dark bg-dark mb-5">
-            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile Generator</span>
         </nav>
         <div class="container">
             <div class="row">`;
